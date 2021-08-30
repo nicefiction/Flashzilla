41e4568d53ea05e14a2e4c4186066ed083fab640
Olivier Van hamme <nicefiction@gmail.com>
2021-08-30 08:22:00 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Olivier Van hamme on 30/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
