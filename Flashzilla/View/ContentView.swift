07011345854fe5d12bf6b94ecb0b7077bded93f8
Olivier Van hamme <nicefiction@gmail.com>
2021-09-01 19:43:24 +0200

    Completes Day 88.2 : Builds a stack of cards .

--- Flashzilla/View/ContentView.swift
+++ Flashzilla/View/ContentView.swift
@@ -8,12 +8,54 @@ import SwiftUI
 
 struct ContentView: View {
    
+   // MARK: - PROPERTY WRAPPERS
+   
+   @State private var cards: Array<CardModel> = Array<CardModel>(repeating: CardModel.example,
+                                                                 count: 10)
+   
+   
+   
    // MARK: - COMPUTED PROPERTIES
    
    var body: some View {
       
-      CardView(card: CardModel.example)
+      /// Place our cards and timer on top of a background :
+      ZStack {
+         Image("background")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+         /// Place a timer above our cards :
+         VStack {
+            /// Make our stack of cards partially overlap with a neat 3D effect :
+            ZStack {
+               ForEach(0..<cards.count) { (index: Int) in
+                  CardView(card: cards[index])
+                     .stacked(at: index,
+                              in: cards.count)
+               }
+            }
+         }
+      }
    }
+}
+
+
+
+
+
+// MARK: - EXTENSIONS -
+
+extension View {
+   
+    func stacked(at position: Int,
+                 in total: Int)
+    -> some View {
+      
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0,
+                                  height: offset * 10))
+    }
 }
 
 
